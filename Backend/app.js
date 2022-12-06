f6b98afbea81388a21976bde21c28ab8611b031b
Naveen Patidar <naveenpatidar3331@gmail.com>
2022-12-07 00:48:29 +0530

    initalization

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,17 @@
-let a = "nashe"
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.end("running")
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("Port is running on"+ process.env.PORT )
+})
